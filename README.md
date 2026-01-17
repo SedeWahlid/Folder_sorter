7dda1f9e302a3a0cf1dc356f6fa41566a1de5d66
Wahlid Sedeghi <Sede.wahlid@gmail.com>
2026-01-17 02:58:16 +0100

    Added  features and made changes in functions. Feature added : - more folders that can be sorted - user input for folder choice and file extension choice
    Implementations/changes :
    - added user input functions for choosing the folders and the extension to which we sort for.
    - added a basic console function which prints the possible choices of folders 
    - changed the create_folder function to make directories more dynamically based on user input. Further it now has a return value and 2 arguments.
    - changed move_files function which now has 4 arguments and handles the move more dynamically.

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-#Folder_sorter v0.01
-
-A simple Project to sort all files from specific folders based on extensions. 
+A simple Project to sort/move all files from specific folders based on extensions. 
 
 So basically we sort all files based on there extension and make folders named after the extension in upper case and we put all the files into there specific folder.
 
@@ -9,10 +7,9 @@ Note :
 
 It is still ongoing and therefore not finished !
 
-Furthermore, the current version is just a blueprint and can only sort within the Downloads folder and only pdf files . It also cannot sort nested folder that contain pdf files. And all pdf files are moved into a folder called PDF within the Documents folder.
+Furthermore, the current version is just a blueprint and can only sort within Downloads, Desktop and Documents folder . It also cannot sort nested folder that contain pdf files. And all files are safed/moved within there named extension.
 
 Features to be added :
 
-- user should be able to input which folder shall be sorted and where those files should be moved. 
-- user should be able to only sort specific extension files for example only pdfs or py files etc. 
 - a basic interface for the program 
+- eventually adding a more dynamic solution for users to not be restricted to those 3 folders.
--- main.py
+++ main.py
@@ -6,13 +6,48 @@ import shutil as sl
 
 # Global variables
 DOWNLOAD_PATH = os.path.join(os.path.expanduser("~"), "Downloads")
-DOCUMENT_PATH = os.path.join(os.path.expanduser("~"),"Documents","PDF")
+DOCUMENTS_PATH = os.path.join(os.path.expanduser("~"), "Documents")
+DESKTOP_PATH = os.path.join(os.path.expanduser("~"), "Desktop")
+
+# possible Choices 1: Downloads , 2: Documents, 3: Desktop
+p_choices = {"1": DOWNLOAD_PATH, "2": DOCUMENTS_PATH, "3": DESKTOP_PATH}
+
+# -----------------------------------------------
+
 # -- FUNCTIONS -- 
 
-# function to  get the list of all the current files within the download section 
-def list_files() -> list[str]:
+def console_folder_choice()-> None:
+    print("1: Downloads\n2: Documents\n3. Desktop\n")
+
+# controlling user input for entering index for the desired folder
+def user_input_folder()-> str:
+    try: 
+        console_folder_choice()
+        while True:
+            try: 
+                user_in = input("Enter index\n")
+                if 1 <= int(user_in) <= 3:
+                    break
+                print("Please enter valid index\n")
+            except:
+                print("Please enter valid index\n")
+        
+    except Exception as e :
+        print(e)
+    return p_choices[user_in]
+
+# user input for file extensions
+def user_input_file_extension()-> str:
     try:
-        all_files = os.listdir(DOWNLOAD_PATH)
+        file_ext = input("Enter a extension without the dot, example for .pdf you enter pdf\n")
+    except Exception as e :
+        print(e)
+    return file_ext
+
+# function to  get the list of all the current files within the download section 
+def list_files(users_chosen_path= DOWNLOAD_PATH) -> list[str]:
+    try:
+        all_files = os.listdir(users_chosen_path)
     except PermissionError as e:
         print(e)
     except OSError as e :
@@ -20,31 +55,43 @@ def list_files() -> list[str]:
     return all_files
 
 
-# function to create a folder based on the file extension (For now only pdf files)
-def create_folder() -> None:
+# function to create a folder based on the file extension
+def create_folder(users_choice_move_path, file_ext="PDF") -> None:
     try:
-        os.makedirs(DOCUMENT_PATH,exist_ok=True)
+        os.makedirs(os.path.join(users_choice_move_path, file_ext.upper()),exist_ok=True)
         print("Folder created...")
     except PermissionError as e:
         print(e)
     except OSError as e:
         print(e)
+    return os.path.join(users_choice_move_path, file_ext.upper())
 
-# function to move files to there folder based on extension (for now only pdf files)
-def move_files(all_files: list[str]) -> None :
-    if  os.path.exists(DOCUMENT_PATH):
+# function to move files to there folder based on extension
+def move_files(all_files: list[str], users_choice_target_path, users_choice_move_path, file_ext) -> None :
+    if  os.path.exists(users_choice_move_path):
         try:
             for files in all_files:
-                if files.lower().endswith(".pdf"):
+                if files.lower().endswith("." + file_ext.lower()):
                     try:
-                        sl.move(os.path.join(DOWNLOAD_PATH,files),DOCUMENT_PATH)
+                        sl.move(os.path.join(users_choice_target_path,files),users_choice_move_path)
                     except sl.Error as e:
                         print(e)
         except Exception as e:
             print(e)
 
+# -----------------------------------------------
+
 # -- MAIN OPERATION --
 if __name__ == "__main__":
-    files = list_files()
-    create_folder()
-    move_files(files)
+    # -- User input --
+    print("Which folder to sort ?\n")
+    user_sorting_folder = user_input_folder()
+    print("Which folder to move in ?\n")
+    user_target_folder = user_input_folder()
+    print("Which files should be sorted/moved ?\n")
+    user_sorting_file_extension = user_input_file_extension()
+    
+    # -- The Actual moving/sorting --
+    files = list_files(user_sorting_folder)
+    path_to_move_files = create_folder(user_target_folder,user_sorting_file_extension)
+    move_files(files, user_sorting_folder, path_to_move_files, user_sorting_file_extension)
